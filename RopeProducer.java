58fb04ec8cb5a73e8fc2538f6557bd8e8f1cf7fe
Nicholas Latham <nlatham@ada.gonzaga.edu>
2019-11-15 09:48:46 -0800

    added folder for classes and some more businesses

---
+++ RopeProducer.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class RopeProducer extends Business{
+    RopeProducer(String owner){
+        this.owner = owner;
+        workers = 7;
+        type = "ROPE PRODUCER";
+        moneyNeeded = workers*4;
+        itemsNeeded = new Hashtable<String, Integer>();
+        itemsProduced = "ROPE";
+        amountProduced = 10;  
+
+        itemsNeeded.put("BREAD", 1);
+        itemsNeeded.put("HEMP", 3);
+    }
+}
+
